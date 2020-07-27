6586454311f9951872adbb875ec27db08296215c
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-07-28 02:39:32 +0300

    reformat

--- src/main/helpers/StorageHelper.ts
+++ src/main/helpers/StorageHelper.ts
@@ -1,5 +1,5 @@
-import path = require("path")
-import fs = require("fs")
+import * as path from "path"
+import * as fs from "fs"
 
 export class StorageHelper {
     static storageDir: string = path.resolve(__dirname, "..", "..", "storage")
