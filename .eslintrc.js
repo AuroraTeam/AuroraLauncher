79d6bb8da4bd99b65f2433a6f3c65bd844baff48
IsTopNick <itstopnick@yandex.ru>
2022-10-08 20:51:36 +0300

    fixx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
     env: {
-        es2021: true,
         node: true,
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    overrides: [],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-    },
-    plugins: ["@typescript-eslint"],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
     rules: {
         "@typescript-eslint/no-explicit-any": "off",
-    },
-}
+    }
+};
