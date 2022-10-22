3893c631d4df41272cecc8df52cc8a8d21527eb8
IsTopNick <itstopnick@yandex.ru>
2022-10-22 21:00:21 +0300

    no need to

--- src/components/api/websocket/requests/AuthRequest.ts
+++ src/components/api/websocket/requests/AuthRequest.ts
@@ -14,7 +14,7 @@ export class AuthRequest extends AbstractRequest {
      * @param {WebSocketClient} ws - WebSocketClient - the client that sent the request
      * @returns ResponseResult
      */
-    protected async invoke(
+    async invoke(
         data: AuthRequestData,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         ws: WebSocketClient
