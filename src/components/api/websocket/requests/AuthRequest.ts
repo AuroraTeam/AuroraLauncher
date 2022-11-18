1f007887b3cf6a22ddc4e1915bca0ba0faac31a2
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-11-18 22:24:33 +0300

    Авторизация ачё

--- src/components/api/websocket/requests/AuthRequest.ts
+++ src/components/api/websocket/requests/AuthRequest.ts
@@ -3,25 +3,27 @@ import { AbstractRequest, ResponseResult } from "aurora-rpc-server"
 
 type WebSocketClient = Parameters<AbstractRequest["invoke"]>["1"]
 
-// Сломано :)
+export interface ExtendedWebSocketClient extends WebSocketClient {
+    isAuthed: boolean
+}
 
 export class AuthRequest extends AbstractRequest {
     method = "auth"
 
     /**
      * It takes a login and password, passes them to the auth provider, and returns the result
-     * @param {AuthRequestData} data - AuthRequestData - the data that was sent from the client
-     * @param {WebSocketClient} ws - WebSocketClient - the client that sent the request
+     * @param data - the data that was sent from the client
+     * @param ws - the client that sent the request
      * @returns ResponseResult
      */
     async invoke(
         data: AuthRequestData,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        ws: WebSocketClient
+        ws: ExtendedWebSocketClient
     ): Promise<ResponseResult> {
         const provider = App.AuthManager.getAuthProvider()
         const res = await provider.auth(data.login, data.password)
-        // ws.isAuthed = true
+        ws.isAuthed = true
         return res
     }
 }
