1c2c53c487254bd7eb91720695c2606afd50f5a3
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-11-20 23:50:37 +0300

    Фикс работы декораторов на esbuild

---
+++ build.mjs
@@ -0,0 +1,16 @@
+import { esbuildDecorators } from "@anatine/esbuild-decorators"
+import { build } from "esbuild"
+import minimist from "minimist"
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { _, ...buildArgs } = minimist(process.argv.slice(2))
+
+await build({
+    platform: "node",
+    target: "node18",
+    bundle: true,
+    plugins: [esbuildDecorators()],
+    entryPoints: ["src/app.ts"],
+    outdir: "dist",
+    ...buildArgs,
+})
