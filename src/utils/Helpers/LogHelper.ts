1f2f972f9c324e6a68ed09d0c0f09793adb4a58e
IsTopNick <itstopnick@yandex.ru>
2023-03-18 17:57:32 +0300

    да отрефакторил слегка, а что ты мне сделаешь? я в другом городе!

--- src/utils/Helpers/LogHelper.ts
+++ src/utils/Helpers/LogHelper.ts
@@ -8,99 +8,80 @@ import stripAnsi from "strip-ansi";
 
 import { StorageHelper } from "./StorageHelper";
 
+const LOG_LEVELS = {
+    DEBUG: chalk.green,
+    DEV: chalk.green,
+    ERROR: chalk.red,
+    FATAL: chalk.red,
+    INFO: chalk.cyan,
+    WARN: chalk.yellow,
+};
+
 export class LogHelper {
-    private static readonly isDevEnabled = process.argv.includes("--dev");
-    private static readonly isDebugEnabled =
+    private static readonly isDevEnabled: boolean = process.argv.includes("--dev");
+    private static readonly isDebugEnabled: boolean =
         process.argv.includes("--debug") || process.argv.includes("--dev");
 
-    private static getLogDate() {
-        return new Date()
-            .toISOString()
+    private static getLogFilePath(): string {
+        const dateStr = new Date().toISOString()
             .slice(0, 19)
             .replace(/[-:]/g, ".")
             .replace("T", "-");
+            
+        return resolve(StorageHelper.logsDir, `LauncherServer-${dateStr}.log`);
     }
 
-    private static readonly logFilePath = resolve(
-        StorageHelper.logsDir,
-        `LauncherServer-${this.getLogDate()}.log`
-    );
-
-    private static readonly logFileStream = createWriteStream(
-        this.logFilePath,
-        { flags: "a" }
-    );
+    private static readonly logFileStream = createWriteStream(LogHelper.getLogFilePath(), { flags: "a" });
 
     public static debug(msg: any, ...args: any[]): void {
         if (!this.isDebugEnabled) return;
-        this.log(LogLevel.DEBUG, msg, ...args);
+        this.log("DEBUG", msg, ...args);
     }
 
     public static dev(msg: any, ...args: any[]): void {
         if (!this.isDevEnabled) return;
-        this.log(LogLevel.DEV, msg, ...args);
+        this.log("DEV", msg, ...args);
     }
 
     public static error(msg: any, ...args: any[]): void {
-        this.log(LogLevel.ERROR, msg, ...args);
+        this.log("ERROR", msg, ...args);
     }
 
     public static fatal(msg: any, ...args: any[]): void {
-        this.log(LogLevel.FATAL, msg, ...args);
+        this.log("FATAL", msg, ...args);
         process.abort();
     }
 
     public static info(msg: any, ...args: any[]): void {
-        this.log(LogLevel.INFO, msg, ...args);
+        this.log("INFO", msg, ...args);
     }
 
     public static warn(msg: any, ...args: any[]): void {
-        this.log(LogLevel.WARN, msg, ...args);
+        this.log("WARN", msg, ...args);
     }
 
     public static handleUserPrompt(msg: string) {
         this.saveLog(msg + EOL);
     }
 
-    private static log(level: LogLevel, msg: any, ...args: any[]) {
-        let coloredStr: string = chalk.gray(new Date().toLocaleString());
+    private static log(level: keyof typeof LOG_LEVELS, msg: any, ...args: any[]) {
+        const coloredStr = [
+            chalk.gray(new Date().toLocaleString()),
+            LOG_LEVELS[level](` [${level}] `),
+            msg,
+        ].join("");
 
-        switch (level) {
-            case LogLevel.DEBUG:
-            case LogLevel.DEV:
-                coloredStr += chalk.green(` [${level}] `) + msg;
-                break;
-            case LogLevel.ERROR:
-            case LogLevel.FATAL:
-                coloredStr += chalk.red(` [${level}] ${msg}`);
-                break;
-            case LogLevel.INFO:
-                coloredStr += chalk.cyan(` [${level}] `) + msg;
-                break;
-            case LogLevel.WARN:
-                coloredStr += chalk.yellow(` [${level}] ${msg}`);
-                break;
-        }
         this.raw(coloredStr, ...args);
     }
 
     public static raw(msg: any, ...args: any[]) {
         const message = format(msg, ...args) + EOL;
         process.stdout.write(message);
+
         this.saveLog(stripAnsi(message));
     }
 
     private static saveLog(message: string) {
         this.logFileStream.write(message);
     }
-}
-
-enum LogLevel {
-    DEBUG = "DEBUG",
-    DEV = "DEV",
-    ERROR = "ERROR",
-    FATAL = "FATAL",
-    INFO = "INFO",
-    RAW = "RAW",
-    WARN = "WARN",
 }
