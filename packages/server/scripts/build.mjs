47f9941d18dfd387e8bb4c77662710686fa5a733
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-05-17 23:40:26 +0300

    update server decorators plugin

--- packages/server/scripts/build.mjs
+++ packages/server/scripts/build.mjs
@@ -1,9 +1,12 @@
-import { esbuildDecorators } from "@anatine/esbuild-decorators";
+import { esbuildDecorators } from "@aurora-launcher/esbuild-decorators";
 import { build } from "esbuild";
 import minimist from "minimist";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { _, ...buildArgs } = minimist(process.argv.slice(2));
+
+console.log("Build...");
+console.time("Build successfully");
 
 await build({
     platform: "node",
@@ -15,3 +18,5 @@ await build({
     outdir: "dist",
     ...buildArgs,
 });
+
+console.timeEnd("Build successfully");
