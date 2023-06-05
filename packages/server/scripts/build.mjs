8a6a0b0d50631ca46fbb2bcbe5653e32b9288521
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-06-05 22:34:55 +0300

    parent 0fc8b5c5f339df9772a3be6f2cd1737bae56ae9a author Andrew Molchanov <mr.catmanrus@gmail.com> 1685993695 +0300 committer Andrew Molchanov <mr.catmanrus@gmail.com> 1685994067 +0300 gpgsig -----BEGIN PGP SIGNATURE-----
     iQJLBAABCAA1FiEEUj6srYcNQ/XdmduOfHWqNixcJ/wFAmR+OlQXHG1yLmNhdG1h
     bnJ1c0BnbWFpbC5jb20ACgkQfHWqNixcJ/zeBQ//UoCVtepbCuhtW/YMzdIpfKPU
     UboEGy8NFBMToX7d5t7u442pC6rYuPAZb6Xqr2A7wWLN1lOcYFGaTl0gaLDpCADz
     UgohX6whY4HWET1l7Dm31ebB0RbrN0m1HX2nYDQxFfAlzyHUPscq7SoTJR8n/RhP
     slqHZOlAIZIN8aYng0rNwUfC9UoNP898srnq4fly78KBS2Bjtei3++gqqNxO7HOo
     WWo8/zRh0i++QL1Jvd+lJB5MDJWokZwh6EeH7ks4lYTCAxquBEl4BTK3e7jB1Rom
     O4it7+nw8+tL6DROJuWQYKX/9nqS1WRw9B8TLWsFts+/FegH9bctfwr2M4lWaBtE
     0zl2vUjt7Wc+DZrz70hPfyhZxRykdHB3AMNL/J+unGV4Y+J8q5/opsGCSoNNcIua
     uN26cnotQzSs5aIv716amgdcjNdrhi0zkmqTTX0c+ZD6NEHDkfViJiD0WwRc1E9M
     45+IsOfi0xX9//6X1nJYv807l3VuNlog47ikfPAzXZpADjT1Rr60/dzbSKHTjG8/
     3mq4aGQpWfADHgkJhSNtLtQqTq05JKIEPR1CbAKndv2U1EQYsBqQ+K2PmbxAXgsA
     8tlAbw8czpMpIdceCcMXkwA5/k4mhmGQDsVwpQ8h53LGDLAIkryLKQYSLbpLR9Qs
     gSD5wvrlnvx18UsbD0U=
     =Oaf/
     -----END PGP SIGNATURE-----
    
    parent 0fc8b5c5f339df9772a3be6f2cd1737bae56ae9a
    author Andrew Molchanov <mr.catmanrus@gmail.com> 1685993695 +0300
    committer Andrew Molchanov <mr.catmanrus@gmail.com> 1685994022 +0300
    gpgsig -----BEGIN PGP SIGNATURE-----
    
     iQJLBAABCAA1FiEEUj6srYcNQ/XdmduOfHWqNixcJ/wFAmR+OiYXHG1yLmNhdG1h
     bnJ1c0BnbWFpbC5jb20ACgkQfHWqNixcJ/wkFBAAlgw4bp+H26PcwUsuP2zsjAQt
     65h6821pqtFBfLStoCGhuNRjTKO6FQh+VdbAR3hEdbJQsq+D4pAXMzuYeNkgADIi
     Trk+EeEKz3Gqy40Vfhqx6YYexhYFrFiZgTiZNCbyuOLoDRbH+N88FwSxfhDmxWsR
     DqciUvNaJRZLRsGGW0vD6QPTv+TDYk8Jygr2549ACGGqRYKTQxCFv+yvejbVE0el
     8iSsFBTbovS+UKDVFQOpy3zEahEO4f86dVSKt130QI3ueKh6QxwICLa/M4z6fAjx
     0zH41OXA2derpr2MHdFR0oxo7pUjlM5ThYvw6zdV4x7IS/Kn77axKA+3rdzt/G/A
     VwyjXPEWjeTLNCJKaEYjFAqWP3b/9ihZB2j+4+KQ6OD2C+ao+rHFaqG0H53vIplK
     3nVxlpXNLEqefMjeQu50KAPt/UZ5B4frn4nKXs0L4IYgTCB4XqQx4zlspO0fgW3/
     J1gpoWTK5GOr2A1zgWzOe6tAfjVTprdzQm86Ej5KUTcrHdyJJ3B2pP2L/G+55Kc1
     Al0G5Yq9vKIIR6WGcBfZV95IH5NFgLZu9Hns2NCeRPc6z9z2g0WUdRjUWwfJH2OV
     3FpUTYCswhGS+6HORgDHvqskQLcqKEJIy4kc/LNAaW0RjaZNnyYT+78zjmcKfWCT
     1NerolOPzuw/rQf8N90=
     =wL7+
     -----END PGP SIGNATURE-----
    
    testing deploy
    
    upd builds
    
    так лучше
    
    fix
    
    fix
    
    ну это ржака
    
    ну и вот так
    
    ну я честно хз даже
    
    хз, если не сработает то попробую завтра
    
    ну а почему бы и нет
    
    Бл*ть, а ларчик просто открывался

--- packages/server/scripts/build.mjs
+++ packages/server/scripts/build.mjs
@@ -1,14 +1,11 @@
 import { esbuildDecorators } from "@aurora-launcher/esbuild-decorators";
-import { context } from "esbuild";
+import { build, context } from "esbuild";
 import minimist from "minimist";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { _, watch, ...args } = minimist(process.argv.slice(2));
 
-console.log("Build...");
-console.time("Build successfully");
-
-const ctx = await context({
+const options = {
     platform: "node",
     target: "node18",
     bundle: true,
@@ -17,12 +14,14 @@ const ctx = await context({
     entryPoints: ["src/app.ts"],
     outdir: "dist",
     ...args,
-}).catch(() => process.exit(1));
-console.timeEnd("Build successfully");
+};
 
 if (watch) {
-    console.log("Watching...");
+    const ctx = await context(options).catch(() => process.exit(1));
     await ctx.watch();
 } else {
-    ctx.dispose();
+    console.log("Build...");
+    console.time("Build successfully");
+    await build(options).catch(() => process.exit(1));
+    console.timeEnd("Build successfully");
 }
