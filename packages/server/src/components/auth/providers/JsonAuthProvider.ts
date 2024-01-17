2f0ff94b8acd7cc3064fadd9d7c1e7bd0b9a2b3c
Andrew Molchanov <mr.catmanrus@gmail.com>
2024-01-17 13:51:40 +0300

    minor changes

--- packages/server/src/components/auth/providers/JsonAuthProvider.ts
+++ packages/server/src/components/auth/providers/JsonAuthProvider.ts
@@ -8,6 +8,7 @@ import {
     ProfileResponseData,
     ProfilesResponseData,
 } from "./AuthProvider";
+import { UUIDHelper } from "../../../utils";
 
 export class JsonAuthProvider implements AuthProvider {
     private config: JsonAuthProviderConfig;
@@ -17,36 +18,36 @@ export class JsonAuthProvider implements AuthProvider {
     }
 
     async auth(username: string, password: string): Promise<AuthResponseData> {
-        return await HttpHelper.postJson<AuthResponseData>(new URL(this.config.authUrl), {
+        return await HttpHelper.postJson<AuthResponseData>(this.config.authUrl, {
             username,
             password,
         });
     }
 
     async join(accessToken: string, userUUID: string, serverID: string): Promise<boolean> {
-        return await HttpHelper.postJson<boolean>(new URL(this.config.joinUrl), {
+        return await HttpHelper.postJson<boolean>(this.config.joinUrl, {
             accessToken,
-            userUUID,
+            userUUID: UUIDHelper.getWithDashes(userUUID),
             serverID,
         });
     }
 
     async hasJoined(username: string, serverID: string): Promise<HasJoinedResponseData> {
-        return await HttpHelper.postJson<HasJoinedResponseData>(new URL(this.config.hasJoinedUrl), {
+        return await HttpHelper.postJson<HasJoinedResponseData>(this.config.hasJoinedUrl, {
             username,
             serverID,
         });
     }
 
     async profile(userUUID: string): Promise<ProfileResponseData> {
-        return await HttpHelper.postJson<ProfileResponseData>(new URL(this.config.profileUrl), {
-            userUUID,
+        return await HttpHelper.postJson<ProfileResponseData>(this.config.profileUrl, {
+            userUUID: UUIDHelper.getWithDashes(userUUID),
         });
     }
 
     async profiles(usernames: string[]): Promise<ProfilesResponseData[]> {
         return await HttpHelper.postJson<ProfilesResponseData[]>(
-            new URL(this.config.profilesUrl),
+            this.config.profilesUrl,
             usernames,
         );
     }
