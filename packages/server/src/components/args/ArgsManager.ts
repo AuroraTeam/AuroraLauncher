1cb3b8344212c05a64ecc9ad698ebace961c0b5f
ZelearFox <140787513+zelear@users.noreply.github.com>
2024-05-30 11:39:41 +0500

    Добавил обработку и получение аргументов

--- packages/server/src/components/args/ArgsManager.ts
+++ packages/server/src/components/args/ArgsManager.ts
@@ -1,8 +1,30 @@
 import { Service } from "typedi";
+import { Args } from "@root/utils/interfaces";
+import { ConfigManager } from "@root/components/config";
+import yargs from "yargs";
 
 @Service()
-export class ArgsManager() {
-  #args: ;
+export class ArgsManager {
+  #args: Args;
 
-  constructor() {}
+  constructor() {
+      this.#args = yargs
+        .option('port', {
+            type: 'number',
+            default: ConfigManager.config.api.port
+        })
+        .option('host', {
+            type: 'string',
+            default: ConfigManager.config.api.host
+        })
+      .argv as Args;
+  }
+
+    /**
+     * It returns the args object.
+     * @returns The args object
+     */
+    get args(): Args {
+        return this.#args;
+    }
 }
