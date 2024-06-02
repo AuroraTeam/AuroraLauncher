0d3462e117e7ee1f4964801182da1636de0eb44f
Andrew Molchanov <mr.catmanrus@gmail.com>
2024-06-02 04:18:48 +0300

    fix

--- packages/server/src/components/args/ArgsManager.ts
+++ packages/server/src/components/args/ArgsManager.ts
@@ -1,20 +1,21 @@
-import { Service } from "typedi";
-import { Args } from "@root/utils/interfaces";
 import { ConfigManager } from "@root/components/config";
+import { Args } from "@root/utils/interfaces";
 import { program } from "commander";
+import { Service } from "typedi";
 
 @Service()
 export class ArgsManager {
-  #args: Args;
+    #args: Args;
 
-  constructor(configManager: ConfigManager) {
-    program
-      .option('--host <host>', null, configManager.config.api.host)
-      .option('--port <port>', null, configManager.config.api.port.toString())
-      .parse();
-    
-    this.#args = program.opts();
-  }
+    constructor(configManager: ConfigManager) {
+        program
+            .option("--dev", null, false)
+            .option("--host <host>", null, configManager.config.api.host)
+            .option("--port <port>", null, configManager.config.api.port.toString())
+            .parse();
+
+        this.#args = program.opts();
+    }
 
     /**
      * It returns the args object.
