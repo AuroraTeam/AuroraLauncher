85d7cda98d1a77a22c93d1cf6d08839cde9eeee7
kostya-main <simkin-kostya@mail.ru>
2024-07-18 17:40:57 +0300

    Авто вход в аккаунт

--- packages/server/src/components/api/websocket/requests/AuthRequest.ts
+++ packages/server/src/components/api/websocket/requests/AuthRequest.ts
@@ -1,5 +1,7 @@
 import type { AuthRequestData, AuthResponseData } from "@aurora-launcher/core";
 import type { AuthProvider } from "@root/components/auth/providers";
+import { JsonHelper } from "@aurora-launcher/core";
+import { VerifyManager } from "@root/components";
 import { AbstractRequest } from "aurora-rpc-server";
 import { Inject, Service } from "typedi";
 
@@ -10,7 +12,7 @@ import { VerifyMiddleware } from "./VerifyMiddleware";
 export class AuthWsRequest extends AbstractRequest {
     method = "auth";
 
-    constructor(@Inject("AuthProvider") private authProvider: AuthProvider) {
+    constructor(@Inject("AuthProvider") private authProvider: AuthProvider, private verifyManager: VerifyManager) {
         super();
     }
 
@@ -23,6 +25,8 @@ export class AuthWsRequest extends AbstractRequest {
     @VerifyMiddleware()
     async invoke(data: AuthRequestData, ws: ExtendedWebSocketClient): Promise<AuthResponseData> {
         const res = await this.authProvider.auth(data.login, data.password);
+        const authData = JsonHelper.toJson({"login":data.login, "password":data.password});
+        res.token = this.verifyManager.encryptToken(Buffer.from(authData, 'utf8').toString('hex'));
         ws.isAuthed = true;
         return res;
     }
