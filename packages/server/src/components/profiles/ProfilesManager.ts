868624eb27f1427c141e91f5353d2cf4ed5b41b5
Andrew Molchanov <mr.catmanrus@gmail.com>
2025-02-18 23:04:30 +0300

    Игнор скрытых/системных файлов

--- packages/server/src/components/profiles/ProfilesManager.ts
+++ packages/server/src/components/profiles/ProfilesManager.ts
@@ -27,7 +27,7 @@ export class ProfilesManager {
         LogHelper.info(this.langManager.getTranslate.ProfilesManager.sync);
 
         for (const file of files) {
-            if (file.includes(".DS_Store")) continue;
+            if (file.startsWith(".")) continue;
 
             try {
                 const data = await fs.readFile(resolve(StorageHelper.profilesDir, file), "utf-8");
